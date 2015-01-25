0a31364403c0f745cd4df9dbcf90a5dd5de531ba
ffomezolam <ffomezolam@gmail.com>
2015-01-25 17:17:04 -0500

    initial commit

---
+++ antigen.config.sh
@@ -0,0 +1,16 @@
+source ~/.config/antigen/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen theme fishy
+
+antigen bundle git
+antigen bundle bobthecow/git-flow-completion
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle npm
+
+antigen bundle brew
+antigen bundle osx
+antigen bundle vi-mode
+
+antigen apply
